890f726329f139885ed92001afb2f7c7a552d526
Jordan Kullmann <jordankullmann@gmail.com>
2021-04-24 09:04:46 -0500

    dog date first commit

---
+++ JS/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+    $(".carousel").carousel( { interval: 4000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+    $('#signUpButton').click(function(){
+        $('#signUpModal').modal('toggle');
+    });
+    $('#loginButton').click(function(){
+        $('#loginModal').modal('toggle');
+    });
+
+});
